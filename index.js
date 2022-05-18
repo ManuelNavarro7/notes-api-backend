b01fc33e0f2d1cbfa6f5945f27f56e83ecfdc3aa
ManuelNavarro7 <manuel.mnavarro@hotmail.com>
2022-05-18 17:33:25 -0300

    3 cambios

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const logger = require('./middlewre/loggerMiddleware')
 app.use(cors())
 app.use(express.json())
 app.use(logger)
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server runin on port ${PORT}`))
 
 let notes = [
